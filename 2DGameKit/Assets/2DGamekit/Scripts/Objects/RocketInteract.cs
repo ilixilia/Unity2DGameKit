39afd385074100dec985cc1e2b75f54d7bae88b4
ilixilia <ilixilia@gmail.com>
2018-11-21 00:00:05 +0300

    Add game project by Unity tutorial

---
+++ 2DGameKit/Assets/2DGamekit/Scripts/Objects/RocketInteract.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketInteract : MonoBehaviour {
+
+    public GameObject currentInterObj = null;
+
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D other) {
+
+        if (other.CompareTag("interObject"))
+        {
+            Debug.Log(other.name);
+            currentInterObj = other.gameObject;
+
+
+
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
